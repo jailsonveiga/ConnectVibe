031018b86eefed15ab5f16875d47e3843e318983
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-30 00:58:19 -0400

    Add @Entity, @Id, @GeneratedValue annotation. Add Constructor, Getter and Setter

--- src/main/java/com/jailsonveiga/connectvibe/models/Waitress.java
+++ src/main/java/com/jailsonveiga/connectvibe/models/Waitress.java
@@ -1,4 +1,64 @@
 package com.jailsonveiga.connectvibe.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//entity, table database
+//Let springBoot know this is model part of database
+@Entity
 public class Waitress {
+    @Id
+    //Let sql to generate the value automatic
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //Properties
+    private Long waitressId;
+    private String waitressName;
+    private String waitressSection;
+    private Integer waitressTable;
+
+    public Waitress() {
+    }
+
+    //Constructor
+    public Waitress(Long waitressId, String waitressName, String waitressSection, Integer waitressTable) {
+        this.waitressId = waitressId;
+        this.waitressName = waitressName;
+        this.waitressSection = waitressSection;
+        this.waitressTable = waitressTable;
+    }
+
+    //Getter and Setter
+    public Long getWaitressId() {
+        return waitressId;
+    }
+
+    public void setWaitressId(Long waitressId) {
+        this.waitressId = waitressId;
+    }
+
+    public String getWaitressName() {
+        return waitressName;
+    }
+
+    public void setWaitressName(String waitressName) {
+        this.waitressName = waitressName;
+    }
+
+    public String getWaitressSection() {
+        return waitressSection;
+    }
+
+    public void setWaitressSection(String waitressSection) {
+        this.waitressSection = waitressSection;
+    }
+
+    public Integer getWaitressTable() {
+        return waitressTable;
+    }
+
+    public void setWaitressTable(Integer waitressTable) {
+        this.waitressTable = waitressTable;
+    }
 }
