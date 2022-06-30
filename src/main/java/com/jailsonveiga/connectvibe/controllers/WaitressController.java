b98b03b89d6cdde9bd6ec8ae76197b0a351a6e28
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-30 01:33:37 -0400

    Testing the End Point route

--- src/main/java/com/jailsonveiga/connectvibe/controllers/WaitressController.java
+++ src/main/java/com/jailsonveiga/connectvibe/controllers/WaitressController.java
@@ -1,4 +1,20 @@
 package com.jailsonveiga.connectvibe.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+//Listening for request
+@RestController
+@RequestMapping("/waitresses") //plural entity name
 public class WaitressController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> testRoute() {
+        return new ResponseEntity<>("Testing the End point", HttpStatus.OK);
+    }
 }
