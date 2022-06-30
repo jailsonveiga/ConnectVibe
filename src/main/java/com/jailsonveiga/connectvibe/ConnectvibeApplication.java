c98db3242fb38e2d3955aa4e928b14f0e75774dc
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-30 00:22:41 -0400

    Initial commit

---
+++ src/main/java/com/jailsonveiga/connectvibe/ConnectvibeApplication.java
@@ -0,0 +1,13 @@
+package com.jailsonveiga.connectvibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectvibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectvibeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jailsonveiga/connectvibe/ConnectvibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jailsonveiga.connectvibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectvibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
