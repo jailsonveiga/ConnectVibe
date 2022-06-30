9ca7288a7ddf3b953b76b81f34f656ec8aea7512
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-30 02:10:48 -0400

    Create Post Request and save the data in database table

--- src/main/java/com/jailsonveiga/connectvibe/controllers/WaitressController.java
+++ src/main/java/com/jailsonveiga/connectvibe/controllers/WaitressController.java
@@ -1,11 +1,11 @@
 package com.jailsonveiga.connectvibe.controllers;
 
+import com.jailsonveiga.connectvibe.models.Waitress;
+import com.jailsonveiga.connectvibe.repositories.WaitressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 //Listening for request
@@ -13,8 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/waitresses") //plural entity name
 public class WaitressController {
 
+    @Autowired
+    private WaitressRepository waitressRepository;
+
     @GetMapping("/test")
     public ResponseEntity<String> testRoute() {
         return new ResponseEntity<>("Testing the End point", HttpStatus.OK);
+    }
+
+    //Create, Read, Update, Delete
+    //Create = POST
+    //Read = GET
+    //Update = PUT*POST
+    //Delete = DELETE
+
+    @PostMapping("/")
+    public ResponseEntity<Waitress> createWaitress(@RequestBody Waitress newWaitress) {
+        Waitress waitress = waitressRepository.save(newWaitress);
+
+        return new ResponseEntity<>(waitress, HttpStatus.CREATED);
     }
 }
