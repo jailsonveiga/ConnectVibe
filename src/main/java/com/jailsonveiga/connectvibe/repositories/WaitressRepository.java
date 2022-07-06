495a05b96dbfa263c40402ad54d6326a20c18b72
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-06 19:26:25 -0400

    Add WaitressRepository.java

--- src/main/java/com/jailsonveiga/connectvibe/repositories/WaitressRepository.java
+++ src/main/java/com/jailsonveiga/connectvibe/repositories/WaitressRepository.java
@@ -3,6 +3,9 @@ package com.jailsonveiga.connectvibe.repositories;
 import com.jailsonveiga.connectvibe.models.Waitress;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 //Interface is like a contract, any class that implement interface will build the class for us
 public interface WaitressRepository extends JpaRepository<Waitress, Long> {
+//    List<Waitress> findAllByName(String waitressName);
 }
