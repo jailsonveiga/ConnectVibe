fe0c80f1e2e193f9a1b4dba3162b9a8a19a79ad6
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-07 19:15:55 -0400

    Use ManyToOne annotation

--- src/main/java/com/jailsonveiga/connectvibe/models/OrderMenu.java
+++ src/main/java/com/jailsonveiga/connectvibe/models/OrderMenu.java
@@ -1,4 +1,27 @@
 package com.jailsonveiga.connectvibe.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+
+import javax.persistence.*;
+
+@Entity
 public class OrderMenu {
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderMenuId;
+    private String orderMenuItem;
+    private String orderMenuDescription;
+    private Integer orderMenuQuantity;
+    //private orderMenuImage;
+    private Float orderMenuPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", referencedColumnName = "restaurantId")
+    @JsonIgnoreProperties("restaurantId")
+    private Restaurant creator;
+
+
+
 }
