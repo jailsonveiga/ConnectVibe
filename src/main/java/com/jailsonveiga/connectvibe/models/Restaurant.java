a01eaaf2075ab29884cdca8826ed9b36a145b4aa
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-07 19:48:26 -0400

    Restaurant.java Update

--- src/main/java/com/jailsonveiga/connectvibe/models/Restaurant.java
+++ src/main/java/com/jailsonveiga/connectvibe/models/Restaurant.java
@@ -1,4 +1,73 @@
 package com.jailsonveiga.connectvibe.models;
 
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
 public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long restaurantId;
+    private String restaurantName;
+    private String restaurantAddress;
+    private String restaurantPhone;
+
+    @OneToMany(mappedBy = "restaurant", fetch = FetchType.LAZY)
+    private Set<OrderMenu> ordermenus;
+
+    //Default Constructor
+
+    public Restaurant() {
+    }
+
+    //Constructor
+    public Restaurant(Long restaurantId, String restaurantName, String restaurantAddress, String restaurantPhone) {
+        this.restaurantId = restaurantId;
+        this.restaurantName = restaurantName;
+        this.restaurantAddress = restaurantAddress;
+        this.restaurantPhone = restaurantPhone;
+    }
+
+    //Getter and Setter
+    public Set<OrderMenu> getOrdermenus() {
+        return ordermenus;
+    }
+
+    public void setOrdermenus(Set<OrderMenu> ordermenus) {
+        this.ordermenus = ordermenus;
+    }
+
+    public Long getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(Long restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getRestaurantAddress() {
+        return restaurantAddress;
+    }
+
+    public void setRestaurantAddress(String restaurantAddress) {
+        this.restaurantAddress = restaurantAddress;
+    }
+
+    public String getRestaurantPhone() {
+        return restaurantPhone;
+    }
+
+    public void setRestaurantPhone(String restaurantPhone) {
+        this.restaurantPhone = restaurantPhone;
+    }
 }
