d382312869f9e271e27b9a563835103465ee38d5
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-07 19:48:40 -0400

    OrderMenu.java Update

--- src/main/java/com/jailsonveiga/connectvibe/models/OrderMenu.java
+++ src/main/java/com/jailsonveiga/connectvibe/models/OrderMenu.java
@@ -1,7 +1,6 @@
 package com.jailsonveiga.connectvibe.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonIncludeProperties;
 
 import javax.persistence.*;
 
@@ -18,10 +17,69 @@ public class OrderMenu {
     private Float orderMenuPrice;
 
     @ManyToOne
-    @JoinColumn(name = "restaurant_id", referencedColumnName = "restaurantId")
+    @JoinColumn(name = "Restaurant_id", referencedColumnName = "restaurantId")
     @JsonIgnoreProperties("restaurantId")
-    private Restaurant creator;
+    private Restaurant restaurant;
 
+    public OrderMenu() {
+    }
 
+    //Constructor
+    public OrderMenu(Long orderMenuId, String orderMenuItem, String orderMenuDescription, Integer orderMenuQuantity, Float orderMenuPrice, Restaurant restaurant) {
+        this.orderMenuId = orderMenuId;
+        this.orderMenuItem = orderMenuItem;
+        this.orderMenuDescription = orderMenuDescription;
+        this.orderMenuQuantity = orderMenuQuantity;
+        this.orderMenuPrice = orderMenuPrice;
+        this.restaurant = restaurant;
+    }
 
+    //Getter and Setter
+    public Long getOrderMenuId() {
+        return orderMenuId;
+    }
+
+    public void setOrderMenuId(Long orderMenuId) {
+        this.orderMenuId = orderMenuId;
+    }
+
+    public String getOrderMenuItem() {
+        return orderMenuItem;
+    }
+
+    public void setOrderMenuItem(String orderMenuItem) {
+        this.orderMenuItem = orderMenuItem;
+    }
+
+    public String getOrderMenuDescription() {
+        return orderMenuDescription;
+    }
+
+    public void setOrderMenuDescription(String orderMenuDescription) {
+        this.orderMenuDescription = orderMenuDescription;
+    }
+
+    public Integer getOrderMenuQuantity() {
+        return orderMenuQuantity;
+    }
+
+    public void setOrderMenuQuantity(Integer orderMenuQuantity) {
+        this.orderMenuQuantity = orderMenuQuantity;
+    }
+
+    public Float getOrderMenuPrice() {
+        return orderMenuPrice;
+    }
+
+    public void setOrderMenuPrice(Float orderMenuPrice) {
+        this.orderMenuPrice = orderMenuPrice;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
 }
