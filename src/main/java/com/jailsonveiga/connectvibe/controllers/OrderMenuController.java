7528b2e60a96bc0a243c3ec2dac9b7a7c394926a
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-08 22:42:04 -0400

    OrderMenuController.java "Update"

--- src/main/java/com/jailsonveiga/connectvibe/controllers/OrderMenuController.java
+++ src/main/java/com/jailsonveiga/connectvibe/controllers/OrderMenuController.java
@@ -1,13 +1,14 @@
 package com.jailsonveiga.connectvibe.controllers;
 
+import com.jailsonveiga.connectvibe.models.OrderMenu;
+import com.jailsonveiga.connectvibe.models.Restaurant;
 import com.jailsonveiga.connectvibe.repositories.OrderMenuRepository;
+import com.jailsonveiga.connectvibe.repositories.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @CrossOrigin
 @RestController
@@ -17,8 +18,25 @@ public class OrderMenuController {
     @Autowired
     private OrderMenuRepository orderMenuRepository;
 
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
     @GetMapping("/test")
     public ResponseEntity<String> testRoute() {
         return new ResponseEntity<>("OrderMenu Route", HttpStatus.OK);
     }
+
+    @PostMapping("/{restaurantId}")
+    public ResponseEntity<OrderMenu> createOrder(@PathVariable Long restaurantId, @RequestBody OrderMenu newOrderMenu) {
+
+        Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        newOrderMenu.setRestaurant(restaurant);
+
+        OrderMenu orderMenu = orderMenuRepository.save(newOrderMenu);
+
+        return new ResponseEntity<>(orderMenu, HttpStatus.CREATED);
+    }
+
 }
