652a808929a2a10e664bd62bc4fa512ab75a112e
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-08 23:34:16 -0400

    OrderMenuRepository.java "Update"

--- src/main/java/com/jailsonveiga/connectvibe/repositories/OrderMenuRepository.java
+++ src/main/java/com/jailsonveiga/connectvibe/repositories/OrderMenuRepository.java
@@ -3,5 +3,9 @@ package com.jailsonveiga.connectvibe.repositories;
 import com.jailsonveiga.connectvibe.models.OrderMenu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderMenuRepository extends JpaRepository<OrderMenu, Long> {
+
+    List<OrderMenu> findAllByRestaurant_restaurantId(Long restaurantId);
 }
